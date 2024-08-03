3959a6c3f12475a1483ea3183d892b9e73caff41
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-08-03 15:59:25 +0530

    bug fixed

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express=require("express");
 const app=express();
 const cookieParser=require("cookie-parser")
-const mainRouter=require("./routes/index.routes") 
+const mainRouter=require("./routes/index.routes")
+const cors=require('cors');
+
+const corsOptions = {
+    origin: 'https://my-app-client.onrender.com',
+    optionsSuccessStatus: 200
+  };
+  
+app.use(cors(corsOptions)); 
 
 app.use(express.json());
 app.use(cookieParser());
